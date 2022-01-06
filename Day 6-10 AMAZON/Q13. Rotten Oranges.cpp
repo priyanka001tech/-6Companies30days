9fc9da09cd80adc2def129ae978019b447ab31d6
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-01-06 18:23:45 +0530

    Create Q13. Rotten Oranges.cpp

---
+++ Day 6-10 AMAZON/Q13. Rotten Oranges.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        if(grid.empty()) return 0;
+        int n=grid.size(),m=grid[0].size();
+        int t=0,c=0,days=0;
+        
+        queue<pair<int,int>> q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]!=0) t++;
+                if(grid[i][j]==2) q.push({i,j});
+            }
+        }
+        
+        int dx[4]={0,0,1,-1};
+        int dy[4]={1,-1,0,0};
+        
+        while(!q.empty()){
+            int k=q.size();
+            c+=k;
+            while(k--){
+                int x=q.front().first,y=q.front().second;
+                q.pop();
+                for(int i=0;i<4;i++){
+                    int nx=x+dx[i],ny=y+dy[i];
+                    if(nx<0 || ny <0 || nx>=n || ny>=m || grid[nx][ny]!=1){
+                        continue;
+                    }
+                    grid[nx][ny]=2;
+                    q.push({nx,ny});
+                }
+            }
+            if(!q.empty()) days++;
+        }
+        return t==c ? days : -1;
+        
+    }
+};
