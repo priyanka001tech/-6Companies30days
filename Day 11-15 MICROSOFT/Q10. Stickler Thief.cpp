85aa05364afd02ee7ddc05ccd7b0ea6e352562ac
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-01-21 18:43:33 +0530

    Create Q10. Stickler Thief.cpp

---
+++ Day 11-15 MICROSOFT/Q10. Stickler Thief.cpp
@@ -0,0 +1,37 @@
+class Solution
+{
+    public:
+    //Function to find the maximum money the thief can get.
+    int FindMaxSum(int arr[], int n)
+    {
+        //storing sum up to current element including and 
+        //excluding it in respective variables.
+        int incl_curr = arr[0]; 
+        int excl_curr = 0; 
+        
+        //storing sum up to previous element including and 
+        //excluding it in respective variables.
+        int incl_prev = incl_curr; 
+        int excl_prev = excl_curr; 
+        
+        for (int i = 1; i < n; i++)
+        {
+            //updating sum up to current element excluding it as maximum
+            //of sum upto previous element excluding and including it.
+            excl_curr = max(incl_prev, excl_prev); 
+            
+            //updating sum up to current element including it as sum up to 
+            //previous element excluding it + current element.
+            incl_curr = excl_prev + arr[i]; 
+            
+            //updating sum upto previous element including and 
+            //excluding it for next iteration.
+            excl_prev = excl_curr;  
+            incl_prev = incl_curr; 
+        }
+        
+        //returning the maximum of sum up to current element  
+        //including and excluding it.
+        return max(excl_curr, incl_curr);
+    }
+};
