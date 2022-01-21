24e95ff33a06d68df312cb17c960438cafe9db42
Priyanka Kothari <70838173+priyanka001tech@users.noreply.github.com>
2022-01-21 18:46:32 +0530

    Create Q14. Minimum steps to destination.cpp

---
+++ Day 11-15 MICROSOFT/Q14. Minimum steps to destination.cpp
@@ -0,0 +1,14 @@
+
+class Solution{
+public:
+    int minSteps(int D){
+        int target = abs(D), sum = 0, step = 0;
+        
+        while(sum < target || (sum-target)%2 != 0)
+        {
+            step++;
+            sum += step;
+        }
+        return step;
+    }
+};
